a74343ba2f502642fea4b4a74383c2ecf3e0a0a4
Bhavuk <bhavuksharma2202@gmail.com>
2022-08-23 16:05:09 +0530

    Create OptionsMenu and ProfileUpdateActivity

--- app/src/main/java/com/redheadhammer/friendschat/MainActivity.java
+++ app/src/main/java/com/redheadhammer/friendschat/MainActivity.java
@@ -1,14 +1,49 @@
 package com.redheadhammer.friendschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.redheadhammer.friendschat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+        } else if (item.getItemId() == R.id.profile) {
+                Intent intent1 = new Intent(MainActivity.this, ProfileUpdateActivity.class);
+                startActivity(intent1);
+        }
+        return true;
     }
 }
