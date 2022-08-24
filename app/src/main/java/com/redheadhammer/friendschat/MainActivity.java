a4ce8e2c12fbfad186761889224034186ea30f19
Bhavuk <bhavuksharma2202@gmail.com>
2022-08-24 17:32:29 +0530

    Setting Adapter for Main Activity

--- app/src/main/java/com/redheadhammer/friendschat/MainActivity.java
+++ app/src/main/java/com/redheadhammer/friendschat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.redheadhammer.friendschat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,29 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.redheadhammer.friendschat.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private String username;
+    private RecyclerItemView itemViewAdapter;
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final DatabaseReference databaseReference = database.getReference();
+    private final FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+    private final List<String> userList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +42,63 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        getUsername();
+        getUsers();
 
+        itemViewAdapter = new RecyclerItemView(userList, username, this);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.recycler.setAdapter(itemViewAdapter);
+    }
+
+    private void getUsername() {
+        databaseReference.child("Users").child(firebaseUser.getUid()).child("username").addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        username = String.valueOf(snapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                }
+        );
+    }
+
+    private void getUsers() {
+        databaseReference.child("Users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String key = snapshot.getKey();
+                assert firebaseUser != null;
+                if (! firebaseUser.getUid().equals(key)) {
+                    userList.add(key);
+                    itemViewAdapter.notifyItemInserted(userList.size()-1);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
